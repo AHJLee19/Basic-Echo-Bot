ca9c68671f794f36bd8c529edd2ac681e646c70d
Austin Lee <lee4910@purdue.edu>
2025-11-20 20:22:34 -0500

    Added bot reaction to user input.

--- mirror_bot.py
+++ mirror_bot.py
@@ -8,4 +8,15 @@ if 'messages' not in st.session_state:
     st.session_state.messages = []
 
 # Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# Bot reaction to user input
+if prompt := st.chat_input("What would you like to say?"):
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    # add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
 
